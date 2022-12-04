03c3efdc62e1923383d9ce0b402403b54f78a091
yuanqinglong <yuanql96@yeah.net>
2022-12-04 16:23:56 +0800

    fix(content): bug修复， 使用grayscale(0)替换none

--- content.tsx
+++ content.tsx
@@ -4,26 +4,29 @@ export const config: PlasmoContentScript = {
   matches: ["<all_urls>"]
 }
 
-const LEVEL = 4
+const LEVEL = 6
 
 /**
  * 去掉css filter: grayScale
  */
 const loop = (nodes: HTMLCollection, level = 1) => {
-  if (!nodes.length || level > LEVEL) {
+  if (level > LEVEL || !nodes.length) {
     return
   }
+
+  level++
+
   Array.from(nodes).forEach((element) => {
     const styles = getComputedStyle(element)
-    if (styles.filter) {
-      element.setAttribute("style", "filter: none !important;")
-      element.setAttribute("style", "-webkit-filter: none !important;")
+    console.log(level, element, styles.filter)
+    if (styles.filter && /grayscale/.test(styles.filter)) {
+      element.setAttribute("style", "filter: grayscale(0) !important;")
+      element.setAttribute("style", "-webkit-filter: grayscale(0) !important;")
     }
+    loop(element.children, level)
   })
 }
 
 try {
-  console.log(document.children)
-
   loop(document.children)
 } catch {}
