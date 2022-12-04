76b4909e57736e36e0fade2c356f14e468ee67d2
yuanqinglong <yuanql96@yeah.net>
2022-12-04 16:34:53 +0800

    chore(content): delete console

--- content.tsx
+++ content.tsx
@@ -18,7 +18,6 @@ const loop = (nodes: HTMLCollection, level = 1) => {
 
   Array.from(nodes).forEach((element) => {
     const styles = getComputedStyle(element)
-    console.log(level, element, styles.filter)
     if (styles.filter && /grayscale/.test(styles.filter)) {
       element.setAttribute("style", "filter: grayscale(0) !important;")
       element.setAttribute("style", "-webkit-filter: grayscale(0) !important;")
